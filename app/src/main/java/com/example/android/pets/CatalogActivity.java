0ff21c8d6cacb03d21158b92ade88f2a1967744e
John Young <ynyjcxyz@gmail.com>
2022-03-23 17:50:03 -0700

    update the UI and CursorAdapter, add emptyView for the ListView

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -15,23 +15,19 @@
  */
 package com.example.android.pets;
 
-import android.annotation.SuppressLint;
-import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.android.pets.data.PetContract;
 import com.example.android.pets.data.PetContract.PetEntry;
-import com.example.android.pets.data.PetDbHelper;
-import com.example.android.pets.data.PetProvider;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
@@ -101,45 +97,11 @@ public class CatalogActivity extends AppCompatActivity {
                 null,
                 null);
 
-        TextView displayView = findViewById(R.id.text_view_pet);
-        try {
-            // Create a header in the Text View that looks like this:
-            //
-            // The pets table contains <number of rows in Cursor> pets.
-            // _id - name - breed - gender - weight
-            //
-            // In the while loop below, iterate through the rows of the cursor and display
-            // the information from each column in this order.
-            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-            displayView.append(PetEntry._ID + " - " +
-                    PetEntry.COLUMN_PET_NAME + " - " +
-                    PetEntry.COLUMN_PET_BREED + " - " +
-                    PetEntry.COLUMN_PET_GENDER + " - " +
-                    PetEntry.COLUMN_PET_WEIGHT + "\n");
-
-            // Figure out the index of each column
-
-            // Iterate through all the returned rows in the cursor
-            while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" +
-                        cursor.getInt(cursor.getColumnIndex(PetEntry._ID))
-                        + " - " +
-                        cursor.getString(cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME))
-                        + " - " +
-                        cursor.getString(cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED))
-                        + " - " +
-                        cursor.getInt(cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER))
-                        + " - " +
-                        cursor.getInt(cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT))));
-            }
-        } finally {
-            cursor.close();
-        }
-        // Always close the cursor when you're done reading from it. This releases all its
-        // resources and makes it invalid.
+        ListView pet_list = findViewById(R.id.pet_list);
+        PetCursorAdapter adapter = new PetCursorAdapter(this,cursor);
+        pet_list.setAdapter(adapter);
+        View emptyView = findViewById(R.id.empty_view);
+        pet_list.setEmptyView(emptyView);
     }
 
     /**
--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,9 +2,13 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract;
 
 /**
  * {@link PetCursorAdapter} is an adapter for a list or grid view
@@ -17,10 +21,10 @@ public class PetCursorAdapter extends CursorAdapter {
      * Constructs a new {@link PetCursorAdapter}.
      *
      * @param context The context
-     * @param c       The cursor from which to get the data.
+     * @param cursor       The cursor from which to get the data.
      */
-    public PetCursorAdapter(Context context, Cursor c) {
-        super(context, c, 0 /* flags */);
+    public PetCursorAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0 /* flags */);
     }
 
     /**
@@ -35,7 +39,7 @@ public class PetCursorAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         // TODO: Fill out this method and return the list item view (instead of null)
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     /**
@@ -51,5 +55,13 @@ public class PetCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // TODO: Fill out this method
+        // Find fields to populate in inflated template
+        TextView name = view.findViewById(R.id.name);
+        TextView summary = view.findViewById(R.id.summary);
+        // Extract properties from cursor
+        name.setText(cursor.getString(
+                cursor.getColumnIndexOrThrow(PetContract.PetEntry.COLUMN_PET_NAME)));
+        summary.setText(cursor.getString(
+                cursor.getColumnIndexOrThrow(PetContract.PetEntry.COLUMN_PET_BREED)));
     }
 }
